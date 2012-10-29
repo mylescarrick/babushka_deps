0719af4ab46827aaaffa9b8a7eb85986fc43d4f4
Myles Carrick <myles@mylescarrick.com>
2012-10-29 14:40:04 +1100

    initial non-working dep

---
+++ perkins.rb
@@ -0,0 +1,44 @@
+dep 'perkins app', :env, :host, :domain, :app_user, :app_root, :key do
+  def db_name
+    YAML.load_file(app_root / 'config/database.yml')[env.to_s]['database']
+  end
+
+  requires [
+    'ssl certificate'.with(env, domain, 'newington.nsw.edu.au'),
+
+    'benhoskings:rails app'.with(
+      :env => env,
+      :listen_host => host,
+      :domain => domain,
+      :username => app_user,
+      :path => app_root,
+      :data_required => 'no'
+    ),
+
+    # Replace the default config with our own.
+    'conversation:vhost enabled.nginx'.with(
+      :app_name => 'iam',
+      :env => env,
+      :listen_host => host,
+      :domain => domain,
+      :domain_aliases => 'perkins perkins.newington.nsw.edu.au',
+      :path => app_root,
+      :proxy_host => 'localhost',
+      :proxy_port => 9000
+    ),
+  ]
+end
+
+dep 'perkins packages' do
+  requires [
+    'running.nginx',
+    'perkins common packages',
+  ]
+end
+
+dep 'perkins common packages' do
+  requires [
+    'bundler.gem',
+    'curl.lib',
+  ]
+end
